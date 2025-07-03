443dd0b72e0d4ca38bdafac9d491d215df50f441
Travis James <travis@tribehealthsolutions.com>
2025-07-03 09:54:54 -0500

    openrouter

---
+++ app/types/build-constants.d.ts
@@ -0,0 +1,16 @@
+// Build-time constants injected by Vite
+declare const __APP_VERSION: string;
+declare const __COMMIT_HASH: string;
+declare const __GIT_BRANCH: string;
+declare const __GIT_COMMIT_TIME: string;
+declare const __GIT_AUTHOR: string;
+declare const __GIT_EMAIL: string;
+declare const __GIT_REMOTE_URL: string;
+declare const __GIT_REPO_NAME: string;
+declare const __PKG_NAME: string;
+declare const __PKG_DESCRIPTION: string;
+declare const __PKG_LICENSE: string;
+declare const __PKG_DEPENDENCIES: string;
+declare const __PKG_DEV_DEPENDENCIES: string;
+declare const __PKG_PEER_DEPENDENCIES: string;
+declare const __PKG_OPTIONAL_DEPENDENCIES: string;
